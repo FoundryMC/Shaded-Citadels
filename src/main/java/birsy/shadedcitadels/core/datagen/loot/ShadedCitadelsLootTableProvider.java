791681311df33ec0efa1f54ac005ea455ec1f63b
birsy <evan.c.muncy@gmail.com>
2023-03-04 15:31:37 -0500

    accidentally broke loot tables...

--- src/main/java/birsy/shadedcitadels/core/datagen/loot/ShadedCitadelsLootTableProvider.java
+++ src/main/java/birsy/shadedcitadels/core/datagen/loot/ShadedCitadelsLootTableProvider.java
@@ -2,6 +2,7 @@ package birsy.shadedcitadels.core.datagen.loot;
 
 import birsy.shadedcitadels.core.registry.ShadedCitadelsBlocks;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
@@ -16,11 +17,15 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.LootTables;
+import net.minecraft.world.level.storage.loot.ValidationContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -31,6 +36,8 @@ public class ShadedCitadelsLootTableProvider extends LootTableProvider {
     public ShadedCitadelsLootTableProvider(DataGenerator dataGeneratorIn) {
         super(dataGeneratorIn);
     }
+
+    protected void validate(Map<ResourceLocation, LootTable> map, ValidationContext validationtracker) {}
 
     @Override
     protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootContextParamSet>> getTables() {
